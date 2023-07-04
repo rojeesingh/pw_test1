6db7f6fccae774aaf72907de8e1b51a15ac37be5
Rojee Singh <65804189+rojeesingh@users.noreply.github.com>
2023-07-04 23:39:19 +0530

    Create DifferentIdentifier.java
    to create different data type identifiers

---
+++ DifferentIdentifier.java
@@ -0,0 +1,10 @@
+public class VariableDeclaration {
+    public static void main(String[] args) {
+        int age = 23;
+        double height = 1.55;
+        String name = "Rojee";
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height + " meters");
+    }
+}
